8a40fb8ff45990260e85463ded52f57a95703eaa
Maxim <maxplusim@mail.ru>
2024-09-09 12:03:34 +0300

    Improve paragraph detection

--- layout-recognition/src/main/java/com/duallab/layout/processors/ParagraphProcessor.java
+++ layout-recognition/src/main/java/com/duallab/layout/processors/ParagraphProcessor.java
@@ -6,7 +6,7 @@ import org.verapdf.wcag.algorithms.entities.content.TextBlock;
 import org.verapdf.wcag.algorithms.entities.content.TextChunk;
 import org.verapdf.wcag.algorithms.entities.content.TextColumn;
 import org.verapdf.wcag.algorithms.entities.content.TextLine;
-import org.verapdf.wcag.algorithms.entities.enums.SemanticType;
+import org.verapdf.wcag.algorithms.entities.enums.TextAlignment;
 import org.verapdf.wcag.algorithms.semanticalgorithms.utils.CaptionUtils;
 import org.verapdf.wcag.algorithms.semanticalgorithms.utils.ChunksMergeUtils;
 
@@ -25,9 +25,7 @@ public class ParagraphProcessor {
         for (IObject content : contents) {
             if (content instanceof TextLine) {
                 TextLine currentLine = (TextLine)content;
-                TextLine previousLine = previousBlock.getLastLine();
-                double differentLinesProbability = getDifferentLinesProbability(previousBlock, previousLine, currentLine);
-                if (differentLinesProbability > DIFFERENT_LINES_PROBABILITY) {
+                if (isOneParagraph(previousBlock, currentLine)) {
                     previousBlock.add(currentLine);
                 } else {
                     previousBlock = new TextBlock(currentLine);
@@ -50,15 +48,62 @@ public class ParagraphProcessor {
         return newContents;
     }
 
+    private static boolean isOneParagraph(TextBlock previousBlock, TextLine currentLine) {
+        double differentLinesProbability = getDifferentLinesProbability(previousBlock, previousBlock.getLastLine(), currentLine);
+        if (differentLinesProbability < DIFFERENT_LINES_PROBABILITY) {
+            return false;
+        }
+        TextAlignment textAlignment = ChunksMergeUtils.getAlignment(previousBlock, currentLine);
+        if (textAlignment == null) {
+            if (previousBlock.getLinesNumber() > 1) {
+                return false;
+            }
+            if (previousBlock.getLastLine().getLeftX() < currentLine.getLeftX() && previousBlock.getLastLine().getRightX() < currentLine.getRightX()) {
+                return false;// it's wrong
+//                should check next line?
+            }
+        }
+//        if (previousBlock.getLinesNumber() == 2 && previousBlock.getTextAlignment() == null && textAlignment != null) {
+//            //first line or block maybe not a part of this paragraph
+//        }
+        if (textAlignment != null) {
+            if (textAlignment == TextAlignment.RIGHT && previousBlock.getTextAlignment() == TextAlignment.JUSTIFY) {
+                //it's probably first line of next paragraph 
+                return false;
+            }
+//            if (textAlignment == TextAlignment.LEFT && previousBlock.getTextAlignment() == TextAlignment.JUSTIFY) {
+//                //this is last line of paragraph
+//            } else {
+            if (previousBlock.getLinesNumber() > 1) {
+                previousBlock.setTextAlignment(textAlignment);
+            }
+//            }
+        }
+        return true;
+    }
+
+//    private static double getDifferentLinesProbability(TextBlock previousBlock, TextLine previousLine, TextLine currentLine) {
+//        double differentLinesProbability;
+////        if (previousBlock.getLinesNumber() > 1) {
+//            differentLinesProbability = ChunksMergeUtils.mergeLeadingProbability(previousBlock, currentLine);
+////            differentLinesProbability = ChunksMergeUtils.toParagraphMergeProbability(previousLine, currentLine);
+////        } else {
+//            differentLinesProbability = ChunksMergeUtils.mergeLeadingProbability(previousLine, currentLine);
+////        }
+//        if (!CaptionUtils.areOverlapping(previousLine, currentLine.getBoundingBox())) {
+//            differentLinesProbability = 0d;
+//        }
+//        return differentLinesProbability;
+//    }
+    
     private static double getDifferentLinesProbability(TextBlock previousBlock, TextLine previousLine, TextLine currentLine) {
         double differentLinesProbability;
         if (previousBlock.getLinesNumber() > 1) {
-            differentLinesProbability = ChunksMergeUtils.toParagraphMergeProbability(previousLine, currentLine);
+            differentLinesProbability = ChunksMergeUtils.mergeLeadingProbability(previousBlock, currentLine);
+//            differentLinesProbability = ChunksMergeUtils.toParagraphMergeProbability(previousLine, currentLine);
         } else {
-            differentLinesProbability = ChunksMergeUtils.mergeLeadingProbability(previousLine, currentLine);
-        }
-        if (!CaptionUtils.areOverlapping(previousLine, currentLine.getBoundingBox())) {
-            differentLinesProbability = 0d;
+            differentLinesProbability = CaptionUtils.areOverlapping(previousLine, currentLine.getBoundingBox()) ? 1.0d : 0d;
+            differentLinesProbability *= ChunksMergeUtils.mergeLeadingProbability(previousLine, currentLine);
         }
         return differentLinesProbability;
     }
