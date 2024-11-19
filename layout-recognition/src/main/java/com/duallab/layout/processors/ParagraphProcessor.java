cee98b8d8807749cff1f172fdc6428eed0886a50
Maxim <maxplusim@mail.ru>
2024-11-19 15:45:20 +0300

    Add license

--- layout-recognition/src/main/java/com/duallab/layout/processors/ParagraphProcessor.java
+++ layout-recognition/src/main/java/com/duallab/layout/processors/ParagraphProcessor.java
@@ -1,3 +1,10 @@
+/*
+ * Licensees holding valid commercial licenses may use this software in
+ * accordance with the terms contained in a written agreement between
+ * you and Dual Lab srl. Alternatively, the terms and conditions that were
+ * accepted by the licensee when buying and/or downloading the
+ * software do apply.
+ */
 package com.duallab.layout.processors;
 
 import org.verapdf.wcag.algorithms.entities.IObject;
