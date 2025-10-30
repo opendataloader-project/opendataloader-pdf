f11915d88a6c4aa97c9869a0149672c0ea368021
Maxim <maxplusim@mail.ru>
2025-10-30 19:48:23 +0300

    TaggedPDF. Add TextLineSerializer

---
+++ java/opendataloader-pdf-core/src/main/java/org/opendataloader/pdf/json/serializers/TextLineSerializer.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2025 Hancom Inc.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package org.opendataloader.pdf.json.serializers;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+import org.opendataloader.pdf.json.JsonName;
+import org.verapdf.wcag.algorithms.entities.content.TextLine;
+
+import java.io.IOException;
+
+public class TextLineSerializer extends StdSerializer<TextLine> {
+
+    public TextLineSerializer(Class<TextLine> t) {
+        super(t);
+    }
+
+    @Override
+    public void serialize(TextLine textLine, JsonGenerator jsonGenerator, SerializerProvider serializerProvider)
+            throws IOException {
+        jsonGenerator.writeStartObject();
+        SerializerUtil.writeEssentialInfo(jsonGenerator, textLine, JsonName.TEXT_CHUNK_TYPE);
+        jsonGenerator.writeStringField(JsonName.CONTENT, textLine.getValue());
+        jsonGenerator.writeEndObject();
+    }
+}
