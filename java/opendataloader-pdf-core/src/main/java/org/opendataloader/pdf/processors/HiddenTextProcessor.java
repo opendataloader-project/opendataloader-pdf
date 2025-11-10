faafea8741789f972f51833ee01221f8421aa277
Kakhnovich Raman <roma3122001@gmail.com>
2025-11-10 13:34:15 +0300

    Delete try-catch from findHiddenText()

--- java/opendataloader-pdf-core/src/main/java/org/opendataloader/pdf/processors/HiddenTextProcessor.java
+++ java/opendataloader-pdf-core/src/main/java/org/opendataloader/pdf/processors/HiddenTextProcessor.java
@@ -12,41 +12,32 @@ import org.verapdf.wcag.algorithms.entities.IObject;
 import org.verapdf.wcag.algorithms.entities.content.TextChunk;
 import org.verapdf.wcag.algorithms.semanticalgorithms.consumers.ContrastRatioConsumer;
 
-import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class HiddenTextProcessor {
-
-    protected static final Logger LOGGER = Logger.getLogger(HiddenTextProcessor.class.getCanonicalName());
     private static final double MIN_CONTRAST_RATIO = 1.2d;
 
     public static List<IObject> findHiddenText(String pdfName, List<IObject> contents, boolean isFilterHiddenText,
                                                String password) {
         List<IObject> result = new LinkedList<>();
-        try {
-            ContrastRatioConsumer contrastRatioConsumer = StaticLayoutContainers.getContrastRatioConsumer(pdfName, password, false, null);
-            if (contrastRatioConsumer == null) {
-                return contents;
-            }
-            for (IObject content : contents) {
-                if (content instanceof TextChunk) {
-                    TextChunk textChunk = (TextChunk) content;
-                    contrastRatioConsumer.calculateContrastRatio(textChunk);
-                    if (textChunk.getContrastRatio() < MIN_CONTRAST_RATIO) {
-                        if (!isFilterHiddenText) {
-                            textChunk.setHiddenText(true);
-                        } else {
-                            continue;
-                        }
+        ContrastRatioConsumer contrastRatioConsumer = StaticLayoutContainers.getContrastRatioConsumer(pdfName, password, false, null);
+        if (contrastRatioConsumer == null) {
+            return contents;
+        }
+        for (IObject content : contents) {
+            if (content instanceof TextChunk) {
+                TextChunk textChunk = (TextChunk) content;
+                contrastRatioConsumer.calculateContrastRatio(textChunk);
+                if (textChunk.getContrastRatio() < MIN_CONTRAST_RATIO) {
+                    if (!isFilterHiddenText) {
+                        textChunk.setHiddenText(true);
+                    } else {
+                        continue;
                     }
                 }
-                result.add(content);
             }
-        } catch (IOException e) {
-            LOGGER.log(Level.WARNING, "Failed to find hidden text: " + e.getMessage());
+            result.add(content);
         }
         return result;
     }
