3bf27f5066bffa3aa18527829a436800c05b46ad
Maxim <maxplusim@mail.ru>
2026-02-25 11:37:06 +0300

    Auto-tagging. Fix PDFStreamWriter

--- java/opendataloader-pdf-core/src/main/java/org/opendataloader/pdf/processors/PDFStreamWriter.java
+++ java/opendataloader-pdf-core/src/main/java/org/opendataloader/pdf/processors/PDFStreamWriter.java
@@ -32,62 +32,65 @@ public class PDFStreamWriter {
 
     private void write(Object rawToken, PrintWriter printWriter, OutputStream out) throws IOException {
         if (rawToken instanceof COSArray) {
-            printWriter.write("[");
+            out.write("[".getBytes());
             for (COSObject item : (COSArray) rawToken) {
                 write(item.getDirectBase(), printWriter, out);
             }
-            printWriter.write("]");
-            printWriter.write(" ");
+            out.write("]".getBytes());
+            out.write(" ".getBytes());
         } else if (rawToken instanceof COSDictionary) {
-                printWriter.write("<<");
-                for (Map.Entry<ASAtom, COSObject> item : ((COSDictionary)rawToken).getEntrySet()) {
-                    printWriter.write(item.getKey().toString());
-                    printWriter.write(" ");
-                    write(item.getValue(), printWriter, out);
-                }
-                printWriter.write(">>");
-                printWriter.write(" ");
+            out.write("<<".getBytes());
+            for (Map.Entry<ASAtom, COSObject> item : ((COSDictionary)rawToken).getEntrySet()) {
+                out.write(item.getKey().toString().getBytes());
+                out.write(" ".getBytes());
+                write(item.getValue(), printWriter, out);
+            }
+            out.write(">>".getBytes());
+            out.write(" ".getBytes());
         } else if (rawToken instanceof COSString) {
             COSString string = (COSString) rawToken;
             if (string.isHexadecimal()) {
-                printWriter.write("<");
-                printWriter.write(string.getHexString());
-                printWriter.write(">");
-                printWriter.write(" ");
+                out.write("<".getBytes());
+                out.write(string.getHexString().getBytes());
+                out.write(">".getBytes());
+                out.write(" ".getBytes());
             } else {
-                printWriter.write("(");
+                out.write("(".getBytes());
                 byte[] bytes = ((COSString) rawToken).get();
                 for (byte currentByte : bytes) {
-                    if (currentByte == 40 || currentByte == 41) {
-                        printWriter.write('\\');
+                    if (currentByte == 40 || currentByte == 41 || currentByte == 92) {
+                        out.write('\\');
                     }
-                    printWriter.write(currentByte);
+                    out.write(currentByte);
                 }
-                printWriter.write(")");
-                printWriter.write(" ");
+                out.write(")".getBytes());
+                out.write(" ".getBytes());
             }
         } else if (rawToken instanceof COSBoolean) {
-            printWriter.write(((COSBoolean) rawToken).getBoolean().toString());
-            printWriter.write(" ");
+            out.write(((COSBoolean) rawToken).getBoolean().toString().getBytes());
+            out.write(" ".getBytes());
         } else if (rawToken instanceof COSBase) {
-            printWriter.write(rawToken.toString());
-            printWriter.write(" ");
+            out.write(rawToken.toString().getBytes());
+            out.write(" ".getBytes());
         } else if (rawToken instanceof COSObject) {
             write(((COSObject) rawToken).getDirectBase(), printWriter, out);
         } else if (rawToken instanceof Operator) {
             Operator operator = (Operator) rawToken;
-            printWriter.println(((Operator) rawToken).getOperator());
+            out.write(((Operator) rawToken).getOperator().getBytes());
+            out.write("\n".getBytes());
             if (Operators.BI.equals(operator.getOperator())) {
                 InlineImageOperator inlineImageOperator = (InlineImageOperator) operator;
                 for (Map.Entry<ASAtom, COSObject> item : inlineImageOperator.getImageParameters().getEntrySet()) {
-                    printWriter.write(item.getKey().toString());
-                    printWriter.write(" ");
+                    out.write(item.getKey().toString().getBytes());
+                    out.write(" ".getBytes());
                     write(item.getValue(), printWriter, out);
                 }
-                printWriter.println(Operators.ID);
+                out.write(Operators.ID.getBytes());
+                out.write("\n".getBytes());
                 IOUtils.copy(inlineImageOperator.getImageData(), out);
-                printWriter.write("\n");
-                printWriter.println(Operators.EI);
+                out.write("\n".getBytes());
+                out.write(Operators.EI.getBytes());
+                out.write("\n".getBytes());
             }
         }
     }
