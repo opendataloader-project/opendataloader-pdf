a2efb898ad933c248f8d496fbd2e4092dd0f36fb
Kakhnovich Raman <roma3122001@gmail.com>
2026-06-29 10:45:31 +0300

    Update DocumentProcessor.java

--- java/opendataloader-pdf-core/src/main/java/org/opendataloader/pdf/processors/DocumentProcessor.java
+++ java/opendataloader-pdf-core/src/main/java/org/opendataloader/pdf/processors/DocumentProcessor.java
@@ -612,7 +612,8 @@ public class DocumentProcessor {
         StaticResources.setDocument(pdDocument);
         GFSAPDFDocument document = new GFSAPDFDocument(pdDocument);
 //        org.verapdf.gf.model.impl.containers.StaticContainers.setFlavour(Collections.singletonList(PDFAFlavour.WCAG_2_2));
-        StaticResources.setFlavour(Collections.singletonList(PDFFlavour.WCAG_2_2_PDF_2_0_HUMAN));
+        StaticResources.setFlavour(Collections.singletonList(Objects.equals(pdDocument.getVersion(), 2.0F) ?
+            PDFFlavour.WCAG_2_2_PDF_2_0_HUMAN : PDFFlavour.WCAG_2_2_HUMAN));
         StaticStorages.setIsFilterInvisibleLayers(config.getFilterConfig().isFilterHiddenOCG());
         StaticContainers.setDocument(document);
         if (config.isUseStructTree()) {
